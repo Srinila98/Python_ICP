8fb3333603242f4ac6e03d71cafdffeb3639841a
Srinila98 <98369777+Srinila98@users.noreply.github.com>
2022-01-26 23:36:23 -0600

    Delete Downloads/CSEE5590_python_Icp-master/CSEE5590_python_Icp-master/ICP 1/Source directory

--- Downloads/CSEE5590_python_Icp-master/CSEE5590_python_Icp-master/ICP 1/Source/Icp1.py
+++
@@ -1,8 +0,0 @@
-def replace_string():
-    _input = input("Enter the string:- ")
-    _input = _input.lower()
-    output = _input.replace("python", "pythons")
-    print("The Replaced string is :-", output)
-
-
-replace_string()
